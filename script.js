be3f34cdf503d42ed8b333b3512383222411cbb9
Godisstrut <erikh13255@gmail.com>
2025-04-25 13:11:09 +0200

    API questions
    Created a function for displaying trivia questions, also template for html

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ async function FetchApiCall() {
         const response = await fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple");
         const data = await response.json();
         console.log(data);
+        const question = data.results[0];
+        const answer = data.results[0];
+        DisplayQuestion(question, answer);
     } catch (err) {
         console.error("Failed to fetch data from API", err)
     }
+}
+
+function DisplayQuestion(question, answer) {
+    document.getElementById("questions").textContent = question.question
+    document.getElementById("answers").textContent = answer.correct_answer
+    document.getElementById("answers").textContent = answer.incorrect_answer
 }
 
 FetchApiCall();
