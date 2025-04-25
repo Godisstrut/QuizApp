d9b0cf275562d475f27888c78ca8267550bffc43
Godisstrut <erikh13255@gmail.com>
2025-04-25 17:53:37 +0200

    Update API call
    Added Boolean questions to API

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+let triviaQuestions = [];
+
 async function FetchApiCall() {
     try {
         const response = await fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple");
         const data = await response.json();
+        triviaQuestions = data.results;
         console.log(data);
         const question = data.results[0];
         const allAnswers = [...question.incorrect_answers, question.correct_answer];
         DisplayQuestion(question, allAnswers);
     } catch (err) {
         console.error("Failed to fetch data from API", err)
+    }
+}
+
+async function FetchBooleanQuestion() {
+    try {
+        const response = await fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean");
+        const data = await response.json();
+        triviaQuestions = data.results;
+        const question = data.results[0];
+        const allAnswers = [...question.incorrect_answers, question.correct_answer];
+        console.log(data);
+        DisplayQuestion(question, allAnswers);
+    } catch (err) {
+        console.error("Failed to fetch true or false questions", err)
     }
 }
 
@@ -24,4 +41,4 @@ function DisplayQuestion(question, allAnswers) {
     })
 }
 
-FetchApiCall();
+FetchBooleanQuestion();
